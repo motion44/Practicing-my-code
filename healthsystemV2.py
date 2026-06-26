f34b19670988ec413369457bb1637bf4fbd29e1f
Motion <waaizirfan@gmail.com>
2026-06-26 19:43:22 -0400

    Implement health management game loop

---
+++ healthsystemV2.py
@@ -0,0 +1,27 @@
+health = 10
+
+while health > 0:
+    
+    number = input("Choose a number between 1 and 2: ")
+    number = int(number)
+    
+    if number != 1 and number != 2:
+        print("You chose the wrong number!")
+    
+    if number == 1:
+        choice = input("How much damage? ")
+        choice = int(choice)
+        health = health - choice
+        print(f"Your health is now {health}")
+    
+    elif number == 2:
+        heal = input("How much heal? ")
+        heal = int(heal)
+        health = health + heal
+        if health > 10:
+            print("Your health bar is full!")
+        else:
+            print(f"Your health is now {health}")
+    
+    if health <= 0:
+        print("Game Over!")
