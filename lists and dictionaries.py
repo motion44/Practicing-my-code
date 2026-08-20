d60b8dc6de5258fe90b247690bda47c4c96b5218
Motion <waaizirfan@gmail.com>
2026-08-19 22:20:10 -0400

    Create lists and dictionaries.py

---
+++ lists and dictionaries.py
@@ -0,0 +1,6 @@
+cereal_brands = ["Vectors", "Cheerios,", "Corn Flakes"]
+print(cereal_brands[1])
+
+daily_goal = {"water_goal": 4, "pushups_done": 17,  "meals": ["Rice", "Cereal", "Yogurt"]}
+
+print(daily_goal["meals"][0])
